97774a4b44fb5973371b4180d53efa8989aff185
ayush <ayushtiwariiitr@gmail.com>
2015-02-11 23:08:09 +0530

    Added initial testing views running on bottle

---
+++ views.py
@@ -0,0 +1,8 @@
+from get_data import ratings
+from bottle import route, run, template
+
+@route('/')
+def show_movie_ratings(movies=ratings):
+    return template('index.html')
+
+run(host="localhost", post=8080, debug=True)
